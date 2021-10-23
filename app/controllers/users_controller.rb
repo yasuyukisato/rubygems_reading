3136411d580a63d96c5e75ab4734f54b0204f35e
sum16 <godasper@gmail.com>
2021-10-24 04:54:22 +0900

    追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < Clearance::UsersController
   def new
     @user = user_from_params
     # @userにはUserクラスのインスタンスが入っていた
-    logger.debug(user_from_params)
+    # logger.debug(user_from_params)
     # 結局user_from_paramsがどこから来ているのかは謎
   end
 
@@ -10,6 +10,7 @@ class UsersController < Clearance::UsersController
     @user = user_from_params
     if @user.save
       Delayed::Job.enqueue EmailConfirmationMailer.new(@user.id)  # email_confirmation_mailer.rb 1
+      # 確認メールを送っている
       flash[:notice] = t(".email_sent")
       redirect_back_or url_after_create
     else
